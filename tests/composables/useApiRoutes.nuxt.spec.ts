e99a37f00f80d964ada220d0fac6eb17147c4160
Stephane P. Pericat <contact@pericat.net>
2025-09-10 21:56:12 -0500

    fix: simplify database

--- tests/composables/useApiRoutes.nuxt.spec.ts
+++ tests/composables/useApiRoutes.nuxt.spec.ts
@@ -4,19 +4,16 @@ import { defineEventHandler, readBody } from 'h3'
 import { describe, it, expect } from 'vitest'
 import { useApiRoutes } from '../../composables/useApiRoutes'
 
-registerEndpoint(
+const mockUser = registerEndpoint(
   '/api/user/create-user',
   defineEventHandler(async (event) => {
     const { email } = await readBody(event)
 
     return {
-      id: 1,
+      id: crypto.randomUUID(),
       email,
       role: 'USER',
       profile: null,
-      comments: [],
-      posts: [],
-      reviews: [],
     }
   }),
 )
@@ -26,14 +23,11 @@ describe('Composables > useApiRoutes', () => {
     const { createUser } = useApiRoutes()
     const email = 'johndoe@thatcovid.life'
     const call = await createUser(email)
-    expect(call).toEqual({
-      id: 1,
-      email,
-      role: 'USER',
-      profile: null,
-      comments: [],
-      posts: [],
-      reviews: [],
-    })
+
+    expect(call).toBeDefined()
+    expect(call.email).toBe(email)
+    expect(call.id).toBeDefined()
+    expect(call.role).toBe('USER')
+    expect(call.profile).toBeNull()
   })
 })
