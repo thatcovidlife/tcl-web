6860862de199f5ec288e79d6fd06ece5d6e338d8
Stephane P. Pericat <contact@pericat.net>
2025-10-07 10:41:44 -0500

    chore: cleanup test setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,13 +3,10 @@ import { resolve } from 'node:path'
 
 export default defineVitestConfig({
   test: {
-    alias: {
-      '@prisma/client': resolve('./tests/mocks/prisma.ts'),
-    },
     dir: 'tests',
     coverage: {
       enabled: true,
-      include: ['composables/*.ts', 'pages/**/*.vue'],
+      include: ['composables/*.ts', 'pages/**/*.vue', 'components/*.vue'],
       reportsDirectory: './coverage',
     },
     globals: true,
