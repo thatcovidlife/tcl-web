5c5d263e8913ade89ad45d808795eb5bfd4334fb
Stephane P. Pericat <contact@pericat.net>
2025-10-14 17:43:06 -0500

    feat(tcl-211): add chat page

---
+++ pages/chat.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'chatbot',
+})
+</script>
+<template>
+  <div class="h-full flex flex-col justify-center w-full stretch">Chat</div>
+</template>
