4e7ee3ca035208c005d84cd8f3ca430ba3afc62e
Stephane P. Pericat <contact@pericat.net>
2025-10-14 17:56:01 -0500

    fix: double import issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,14 @@ export default defineNuxtConfig({
     classSuffix: '',
   },
 
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+      ignore: ['**/ai-elements/**/index.ts', '**/ai-elements/**/types.ts'],
+    },
+  ],
+
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
 
