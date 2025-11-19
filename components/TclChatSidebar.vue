aa2b16472d48aaca7e38127cf79cd1b2a9fadf8a
Stephane P. Pericat <contact@pericat.net>
2025-11-19 17:52:50 -0600

    feat: sidebar wrapper

---
+++ components/TclChatSidebar.vue
@@ -0,0 +1,7 @@
+<template>
+  <aside
+    class="h-[calc(100vh-64px)] hidden md:block border-r-muted border-r p-4 bg-muted/25"
+  >
+    sidebar goes here
+  </aside>
+</template>
--- layouts/chatbot.vue
+++ layouts/chatbot.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="font-sans">
-    <TclHeader />
-    <TclBody class="h-[calc(100vh-64px)]">
+    <TclHeader class="!z-20" />
+    <TclBody class="h-[calc(100vh-64px)] grid md:grid-cols-[16rem_1fr]">
+      <TclChatSidebar />
       <NuxtPage />
     </TclBody>
     <Toaster />
