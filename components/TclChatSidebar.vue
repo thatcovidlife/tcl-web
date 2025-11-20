9f7b21e22830bcb58e8b95d267c4c779aea6e423
Stephane P. Pericat <contact@pericat.net>
2025-11-19 19:07:55 -0600

    feat: tablet sidebar

--- components/TclChatSidebar.vue
+++ components/TclChatSidebar.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { PenLine } from 'lucide-vue-next'
 const localePath = useLocalePath()
 const { t } = useI18n()
 
@@ -6,12 +7,20 @@ const emit = defineEmits(['new-chat'])
 </script>
 <template>
   <aside
-    class="h-[calc(100vh-64px)] hidden md:block border-r-muted border-r px-4 py-6 bg-muted/25"
+    class="h-[calc(100vh-64px)] hidden md:block border-r-muted border-r px-4 py-4 lg:py-6 bg-muted/25"
   >
     <TclMoreButton
-      extra="w-full"
+      extra="lg:hidden rounded-full"
+      size="icon"
+      @click="$emit('new-chat')"
+    >
+      <PenLine />
+    </TclMoreButton>
+    <TclMoreButton
+      extra="w-full hidden lg:flex"
       :label="t('chatbot.new')"
       :link="localePath('/chat')"
+      size="lg"
       target="_self"
       @click="$emit('new-chat')"
     />
