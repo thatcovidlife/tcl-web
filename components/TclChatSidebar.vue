b9be51979ebd558ff96d2a934c653e488a11f49f
Stephane P. Pericat <contact@pericat.net>
2025-11-19 19:18:52 -0600

    fix: new chat at bottom

--- components/TclChatSidebar.vue
+++ components/TclChatSidebar.vue
@@ -7,22 +7,25 @@ const emit = defineEmits(['new-chat'])
 </script>
 <template>
   <aside
-    class="h-[calc(100vh-64px)] hidden md:block border-r-muted border-r px-4 py-4 lg:py-6 bg-muted/25"
+    class="h-[calc(100vh-64px)] hidden md:flex border-r-muted border-r px-4 py-4 lg:py-6 bg-muted/25 flex-col items-stretch justify-between"
   >
-    <TclMoreButton
-      extra="lg:hidden rounded-full"
-      size="icon"
-      @click="$emit('new-chat')"
-    >
-      <PenLine />
-    </TclMoreButton>
-    <TclMoreButton
-      extra="w-full hidden lg:flex"
-      :label="t('chatbot.new')"
-      :link="localePath('/chat')"
-      size="lg"
-      target="_self"
-      @click="$emit('new-chat')"
-    />
+    <div>&nbsp;</div>
+    <div>
+      <TclMoreButton
+        extra="lg:hidden rounded-full"
+        size="icon"
+        @click="$emit('new-chat')"
+      >
+        <PenLine />
+      </TclMoreButton>
+      <TclMoreButton
+        extra="w-full hidden lg:flex"
+        :label="t('chatbot.new')"
+        :link="localePath('/chat')"
+        size="lg"
+        target="_self"
+        @click="$emit('new-chat')"
+      />
+    </div>
   </aside>
 </template>
