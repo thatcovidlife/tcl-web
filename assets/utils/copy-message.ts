a37961ea0a0008ac19b344b0eb3cc4ba9c31f358
Stephane P. Pericat <contact@pericat.net>
2025-11-26 21:24:52 -0600

    fix(tcl-221): hide uuids outputed by gpt-oss, both in app and in copy

--- assets/utils/copy-message.ts
+++ assets/utils/copy-message.ts
@@ -1,12 +1,15 @@
-import { marked } from 'marked';
+import { marked } from 'marked'
+import { citationExtension } from './marked-citations'
+
+marked.use({ extensions: [citationExtension] })
 
 export const copyMessage = async (markdown: string) => {
-  const html = await marked(markdown);
-  
+  const html = await marked(markdown)
+
   await navigator.clipboard.write([
     new ClipboardItem({
       'text/plain': new Blob([markdown], { type: 'text/plain' }),
       'text/html': new Blob([html], { type: 'text/html' }),
     }),
-  ]);
+  ])
 }
