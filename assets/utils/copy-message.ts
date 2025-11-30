a0c39c14ac4bee109f48e34df81874975e0cd115
Stephane P. Pericat <contact@pericat.net>
2025-11-30 15:25:26 -0600

    fix(tcl-221): copy text

--- assets/utils/copy-message.ts
+++ assets/utils/copy-message.ts
@@ -4,11 +4,12 @@ import { citationExtension } from './marked-citations'
 marked.use({ extensions: [citationExtension] })
 
 export const copyMessage = async (markdown: string) => {
-  const html = await marked(markdown)
+  const fullMd = `${markdown}\n\n*Copied from That Covid Life Chatbot*`
+  const html = await marked(fullMd)
 
   await navigator.clipboard.write([
     new ClipboardItem({
-      'text/plain': new Blob([markdown], { type: 'text/plain' }),
+      'text/plain': new Blob([fullMd], { type: 'text/plain' }),
       'text/html': new Blob([html], { type: 'text/html' }),
     }),
   ])
