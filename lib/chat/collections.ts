fe34c942126cfdce64263887b49c7f1f411ce378
Stephane P. Pericat <contact@pericat.net>
2026-01-22 09:03:12 -0600

    chore: set up

--- lib/chat/collections.ts
+++ lib/chat/collections.ts
@@ -1,15 +1,15 @@
 export const collections = [
   {
     label: 'General Knowledge',
-    value: 'tcl-chatbot-general-db',
+    value: 'tcl-chatbot-general-db-qwen',
   },
   {
     label: 'Scientific Knowledge (Lancet)',
-    value: 'tcl-chatbot-lancet-db',
+    value: 'tcl-chatbot-lancet-db-qwen',
   },
 ]
 
 export const defaultCollection: string = collections[0].value
 
 export const collectionName = (selection: string) =>
-  `tcl-chatbot-${selection}-db`
+  `tcl-chatbot-${selection}-db-qwen`
