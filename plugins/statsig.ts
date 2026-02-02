ad6d46c819e1e494afbbb0787b2c70ab64b38a9f
Stephane P. Pericat <contact@pericat.net>
2026-02-01 22:31:28 -0600

    fix: statsig stable id

--- plugins/statsig.ts
+++ plugins/statsig.ts
@@ -2,6 +2,7 @@ import { StatsigClient } from '@statsig/js-client'
 import { StatsigSessionReplayPlugin } from '@statsig/session-replay'
 import { StatsigAutoCapturePlugin } from '@statsig/web-analytics'
 import consola from 'consola'
+import { state } from 'happy-dom/lib/PropertySymbol.js'
 
 export default defineNuxtPlugin(async () => {
   if (process.env.NODE_ENV === 'development') {
@@ -9,14 +10,32 @@ export default defineNuxtPlugin(async () => {
   }
 
   const config = useRuntimeConfig()
+  const { user } = useUserSession()
+  const userInfo = useUserStore().info || null
 
   if (!config.public.statsig.clientKey) {
     consola.warn('Statsig client key not found')
   }
 
+  // Get or create stable ID from cookie
+  const stableIdCookie = useCookie('user.stable_id', {
+    maxAge: 60 * 60 * 24 * 400, // 400 days (browser maximum)
+    sameSite: 'lax',
+    secure: true,
+  })
+
+  if (!stableIdCookie.value) {
+    stableIdCookie.value = crypto.randomUUID()
+  }
+
   const myStatsigClient = new StatsigClient(
     config.public.statsig.clientKey,
-    { userID: crypto.randomUUID() }, // TODO: fix
+    {
+      userID: userInfo?.id || user.value?.sub || crypto.randomUUID(),
+      customIDs: {
+        stableID: stableIdCookie.value,
+      },
+    },
     {
       plugins: [
         new StatsigSessionReplayPlugin(),
